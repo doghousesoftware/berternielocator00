166b5f06b28de9565a850a41a3f86b771716650f
bryan weinstein <bryanweinstein@outlook.com>
2021-11-19 11:29:47 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,8 +12,10 @@ radio.onReceivedNumber(function (receivedNumber) {
 function nightAlert () {
     for (let index = 0; index < 4; index++) {
         pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P3, 1)
         basic.pause(500)
         pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P3, 0)
         basic.pause(500)
     }
 }
